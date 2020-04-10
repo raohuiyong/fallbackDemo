d83baea7c1fc3885a794bfde17ee27147e642a59
raohuiyong <raohuiyong@163.com>
2020-04-10 10:24:15 +0800

    测试切换

--- src/components/LocaleProvider/index.tsx
+++ src/components/LocaleProvider/index.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
-import { ConfigProvider } from '@alifd/next';
+import React, { useState, useCallback, useEffect } from "react";
+import { ConfigProvider } from "@alifd/next";
 
+(function () {
+  const throttle = function (type: string, name: string, obj: Window = window) {
+    let running = false;
 
+    const func = () => {
+      if (running) {
+        return;
+      }
+
+      running = true;
+      requestAnimationFrame(() => {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+
+    obj.addEventListener(type, func);
+  };
+
+  throttle("resize", "optimizedResize");
+})();
 interface Props {
   children: React.ReactElement;
 }
+function getDevice(width: number) {
+  const isPhone =
+    typeof navigator !== "undefined" &&
+    navigator &&
+    navigator.userAgent.match(/phone/gi);
 
+  if (width < 680 || isPhone) {
+    return "phone";
+  } else if (width < 1280 && width > 680) {
+    return "tablet";
+  } else {
+    return "desktop";
+  }
+}
 function LocaleProvider(props: Props) {
+  const [device, setDevice] = useState<"phone" | "tablet" | "desktop">(
+    getDevice(NaN)
+  );
   const { children } = props;
 
-
+  const onResize = useCallback((e: any) => {
+    console.log("object");
+    const type = getDevice(e && e.target && e.target.innerWidth);
+    setDevice(type);
+  }, []);
+  useEffect(() => {
+    window.addEventListener("optimizedResize", onResize);
+    return () => {
+      window.removeEventListener("optimizedResize", onResize);
+    };
+  }, []);
   return (
-      <ConfigProvider>
-        {React.Children.only(children)}
-      </ConfigProvider>
+    <ConfigProvider device={device}>
+      {React.Children.only(children)}
+    </ConfigProvider>
   );
 }
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
-import { store as appStore } from "ice";
-import routerConfig from "@/routes";
+import React, { useEffect, useState } from "react";
+import { useRequest } from "ice";
+import axios from "axios";
 
 const Dashboard = () => {
-  const [counterState, counterActions] = appStore.useModel("app");
+  const { data, loading = true, request } = useRequest({
+    url: "/api/projects",
+    method: "GET",
+  });
+  const [source] = useState(axios.CancelToken.source());
   useEffect(() => {
-    counterActions.setRouterList(
-      routerConfig[0].children.filter((item) => item.path === "/about")
-    );
+    request({ data: { name: "raohuiyong" }, cancelToken: source.token });
+    return () => {
+      source.cancel("组件卸载,取消请求");
+    };
   }, []);
   return (
     <>
